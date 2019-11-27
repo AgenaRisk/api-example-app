c7186b28320a762f91a59b2538a742b0739c9780
Eugene Dementiev <dementiev.eugene@gmail.com>
2019-11-27 23:52:42 +0000

    Much better example of simple API interactions with the Asia model

--- src/main/java/example/Demo.java
+++ src/main/java/example/Demo.java
@@ -1,37 +1,57 @@
 package example;
 
-import com.agenarisk.api.exception.AdapterException;
-import com.agenarisk.api.exception.ModelException;
-import com.agenarisk.api.io.JSONAdapter;
-import com.agenarisk.api.io.XMLAdapter;
-import com.agenarisk.api.model.Model;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.stream.Collectors;
+import com.agenarisk.api.model.*;
+import java.io.File;
 
 /**
  *
  * @author Eugene Dementiev
  */
 public class Demo {
-	
-	public static void main(String[] args) throws Exception {
-		String asiaXml = read("Asia.xml");
-		Model model = loadXml(asiaXml);
-		model.calculate();
-		System.out.println(JSONAdapter.toJSONObject(model.getLogicModel()));
-	}
-	
-	private static Model loadXml(String xml) throws ModelException, AdapterException{
-		return Model.createModel(XMLAdapter.xmlToJson(xml));
-	}
-	
-	private static String read(String fileName) throws IOException {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(Demo.class.getResourceAsStream(fileName), Charset.forName("UTF-8")))) {
-			return br.lines().collect(Collectors.joining(System.lineSeparator()));
-		}
-	}
-}
 
+	public static void main(String[] args) throws Exception {
+
+		String modelFilePath = new File(ClassLoader.getSystemClassLoader().getResource("example/Asia.xml").getFile()).getAbsolutePath();
+
+		// Load and calculate the model
+		Model model = Model.loadModel(modelFilePath);
+		model.calculate();
+
+		// We only have one Data Set in the Network, so we can just pick first
+		// Same for network
+		DataSet ds = model.getDataSetList().get(0);
+		Network net = model.getNetworkList().get(0);
+		
+		// We want to see the probability of "yes" for cancer, tuberculosis and bronchitis
+		Node tuberculosis = net.getNode("T");
+		Node cancer = net.getNode("L");
+		Node bronchitis = net.getNode("B");
+		
+		System.out.println("Tuberculosis: " + ds.getCalculationResult(tuberculosis).getResultValue("yes").getValue());
+		System.out.println("Cancer: " + ds.getCalculationResult(cancer).getResultValue("yes").getValue());
+		System.out.println("Bronchitis: " + ds.getCalculationResult(bronchitis).getResultValue("yes").getValue());
+		
+		System.out.println("");
+		
+		// Now we enter some observations into the model and check results again
+		
+		// Smoker
+		ds.setObservation(net.getNode("S"), "yes");
+		
+		// No Asia
+		ds.setObservation(net.getNode("A"), "no");
+		
+		// Positive X-ray
+		ds.setObservation(net.getNode("X"), "yes");
+		
+		model.calculate();
+		
+		System.out.println("Tuberculosis: " + ds.getCalculationResult(tuberculosis).getResultValue("yes").getValue());
+		System.out.println("Cancer: " + ds.getCalculationResult(cancer).getResultValue("yes").getValue());
+		System.out.println("Bronchitis: " + ds.getCalculationResult(bronchitis).getResultValue("yes").getValue());
+		
+		// As expected, probabilty of cancer has grown significantly
+
+	}
+
+}
